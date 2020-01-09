66b49261f644346c0b06ef049a6744720677db73
flash <nexus2493@gmail.com>
2020-01-09 16:48:16 +0900

    static

--- README.md
+++ README.md
@@ -6,8 +6,12 @@
 ## model
 * dir:
 
+## static
+* 정적파일 보관
+
 ## views
-* 
+* dir : views
+* view engine: pug  (퍼그 형태 파일 사용)
 
 ## controller
 
--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
 import express from "express";
-import {join } from "path";
-
+import {join} from "path";
+import socketIO from "socket.io";
 // setting
 const PORT = 4000;
 const app = express();
 
-app.set("views engine", "pug");
+app.set("view engine", "pug");
 app.set("views", join(__dirname, 'views'));
 
+// middle ware 
+app.use(express.static(join(__dirname, "static")));
+
 // routing
-app.get("/",(req,res)=> res.render("home"))
+app.get("/",(req,res)=> res.render("home"));
 
 
 // server start action
